d9240057808c30eb3fe3754b7110a91948df6d2f
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-08-01 17:53:31 +0200

    Adding image domain to env variables

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+let domains = ['127.0.0.1', 'localhost', 'azurite']
+
+if (process.env.NEXT_PUBLIC_IMAGE_HOST !== undefined) {
+    domains.push(process.env.NEXT_PUBLIC_IMAGE_HOST)
+}
+
 const nextConfig = {
     images: {
-        domains: ['127.0.0.1', 'localhost', 'azurite'],
+        domains: domains,
     }
 };
 

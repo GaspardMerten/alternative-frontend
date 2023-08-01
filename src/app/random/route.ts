91133d6669f3367f6c8bad6d3bad9e4af480b963
Gaspard Merten <gaspard.mp.work@gmail.com>
2023-08-01 17:25:49 +0200

    Fixing random route

--- src/app/random/route.ts
+++ src/app/random/route.ts
@@ -1,11 +1,15 @@
 import {allCompaniesSlug} from "@/api/slugs";
 import {NextResponse} from 'next/server'
 
-export async function GET() {
+export async function GET(request: Request) {
     let slugs = await allCompaniesSlug();
 
     let randomSlug = slugs[Math.floor(Math.random() * slugs.length)];
 
-    return NextResponse.redirect(process.env.BASE_URL + '/' + randomSlug)
+    return NextResponse.redirect(
+        request.url.split("://")[0] + '://' + request.headers.get('host') + '/' + randomSlug, {
+            status: 302,
+        }
+    );
 }
 
